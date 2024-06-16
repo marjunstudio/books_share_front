b57df0738152c0643c8d8e5b161d2687954d477e
marjunstudio <uebujie@gmail.com>
2024-06-16 22:19:03 +0900

    fix:ヘッダーレイアウトの修正

--- app/components/layouts/Header/index.tsx
+++ app/components/layouts/Header/index.tsx
@@ -13,21 +13,15 @@ const Header: React.FC = () => {
           </a>
           <ul className="items-stretch hidden space-x-3 lg:flex">
             <li className="flex">
-              <a rel="noopener noreferrer" href="#" className="flex items-center px-4 -mb-1 border-b-2 border-transparent text-purple-600 border-purple-600">Link</a>
+              <a rel="noopener noreferrer" href="/review" className="flex items-center px-4 -mb-1 border-b-2 border-transparent text-purple-600 border-purple-600">レビュー投稿</a>
             </li>
             <li className="flex">
-              <a rel="noopener noreferrer" href="#" className="flex items-center px-4 -mb-1 border-b-2 border-transparent">Link</a>
-            </li>
-            <li className="flex">
-              <a rel="noopener noreferrer" href="#" className="flex items-center px-4 -mb-1 border-b-2 border-transparent">Link</a>
-            </li>
-            <li className="flex">
-              <a rel="noopener noreferrer" href="#" className="flex items-center px-4 -mb-1 border-b-2 border-transparent">Link</a>
+              <a rel="noopener noreferrer" href="/review_list" className="flex items-center px-4 -mb-1 border-b-2 border-transparent">投稿一覧</a>
             </li>
           </ul>
           <div className="items-center flex-shrink-0 hidden lg:flex">
-            <button className="self-center px-8 py-3 rounded">Sign in</button>
-            <button className="self-center px-8 py-3 font-semibold rounded bg-purple-600 text-gray-50">Sign up</button>
+            <button className="self-center px-8 py-3 rounded">ログイン</button>
+            <button className="self-center px-8 py-3 font-semibold rounded bg-purple-600 text-gray-50">新規登録</button>
           </div>
           <button className="p-4 lg:hidden">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6 text-gray-800">
